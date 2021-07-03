7178bb06326f357ed16b7c24a1b981bad02fa0fa
danikaze <danikaze@gmail.com>
2021-07-03 22:33:56 +0900

    Display the current scores in the match list Matches currently playing shouldn't display the final result

--- pages/matches.tsx
+++ pages/matches.tsx
@@ -1,6 +1,8 @@
 import { AppPage, GetServerSideProps } from '@_app';
 import { MatchListPage, Props } from '@components/pages/match-list';
 import { mockMatches } from '@model/match/mock';
+import { MatchSimulatorUpdater } from '@utils/match-simulator/match-simulator-updater';
+import { getSeconds } from '@utils/jikan';
 
 const MatchesPageHandler: AppPage<Props> = (props) => {
   return <MatchListPage {...props} />;
@@ -11,9 +13,22 @@ MatchesPageHandler.defaultProps = {
 };
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const matches = mockMatches.map((match) => {
+    if (match.state !== 'playing') return match;
+    const sim = new MatchSimulatorUpdater(match.log);
+    sim.replay(getSeconds() - match.timestamp);
+    const currentStatus = sim.getResult();
+
+    return {
+      ...match,
+      homeScore: currentStatus.homeScore,
+      awayScore: currentStatus.awayScore,
+    };
+  });
+
   return {
     props: {
-      matches: mockMatches,
+      matches,
     },
   };
 };

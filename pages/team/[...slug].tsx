fa978fc8f0f84f60efcd6c04174a77628b9a8db6
danikaze <danikaze@gmail.com>
2021-09-23 13:57:33 +0900

    Fix: Avoid showing final result in playing matches in the team page

--- pages/team/[...slug].tsx
+++ pages/team/[...slug].tsx
@@ -1,8 +1,10 @@
-import { AppPage, GetServerSideProps } from '@_app';
-import { TeamPage, Props } from '@page-components/team';
-import { mockTeams } from '@model/team/mock';
 import { mockMatches } from '@model/match/mock';
+import { mockTeams } from '@model/team/mock';
+import { Props, TeamPage } from '@page-components/team';
 import { TEAM_UPCOMING_MATCHES_SHOWN } from '@utils/constants/ui';
+import { getMilliseconds } from '@utils/jikan';
+import { simulateOngoingMatch } from '@utils/match-simulator';
+import { AppPage, GetServerSideProps } from '@_app';
 
 const TeamPageHandler: AppPage<Props> = (props) => {
   return <TeamPage {...props} />;
@@ -30,7 +32,21 @@ export const getServerSideProps: GetServerSideProps<Props, Query> = async (
         (match.state === 'pending' || match.state === 'playing') &&
         (match.homeTeam.teamId === teamId || match.awayTeam.teamId === teamId)
     )
-    .slice(0, TEAM_UPCOMING_MATCHES_SHOWN);
+    .slice(0, TEAM_UPCOMING_MATCHES_SHOWN)
+    .map((match) => {
+      if (match.state !== 'playing') return match;
+
+      const currentStatus = simulateOngoingMatch(
+        match,
+        getMilliseconds() - match.timestamp
+      );
+
+      return {
+        ...match,
+        homeScore: currentStatus.homeScore,
+        awayScore: currentStatus.awayScore,
+      };
+    });
 
   return {
     props: {
